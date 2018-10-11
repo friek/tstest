5b87c49ae5e463fa96e9ac3c6fffe4125fcf9fa4
Johan Mulder <johan@mulder.net>
2018-10-11 20:21:50 +0200

    Issueing a window.alert when clicking the h1.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 @click="clicked">{{ msg }}</h1>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -36,6 +36,10 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+
+  public clicked(): void {
+    alert("Clicked");
+  }
 }
 </script>
 
